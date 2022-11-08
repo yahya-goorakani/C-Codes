4757086f8f199dbdf6eec3d9419ba2538bc4f405
yahya <115286845+yahya-goorakani@users.noreply.github.com>
2022-11-09 00:27:46 +0300

    Add files via upload

--- prime/a.c
+++ prime/a.c
@@ -1,97 +1,31 @@
 #include<stdio.h>
-#include<math.h>
+
 #define TRUE 1
 #define FALSE 0
-
-int is_prime(int a);
-
 int main(){
-
-        int a;
-
-        do
-        {
-                printf("enter an integer :");
-                        scanf("%d",&a);
-
-                if(is_prime(a))
-                {
-                printf("%dis a PRIME number.",a);
-                }
-                else
-                {
-                printf("%dis a NOT PRIME number.",a);
-                }
-        printf("\n");
-                }while(a>0);
-
-        return 0;
-}
-int is_prime(int a)
-{
-        int prime=TRUE;
-        int i;
-        for(i=2;i<=sqrt(a);i++)
-        {
-                if(a%i==0)
-                {
-                        prime=FALSE;
-                        break;
-                }
-        }
-        return prime;
-
-}
-
-
-
-
-
-
-/*#include<stdio.h>
-#include<math.h>
-#define TRUE 1
-#define FALSE 0
-
-int is_prime(int a);
-
-int main(){
+	int a;
 	
-        int a;
-	
-        do
-        {
-                printf("enter an integer :");
-                        scanf("%d",&a);
+	printf("ENTER AN INTEGER:");
+	scanf("%d",&a);
+	printf("\n");
 
-                if(is_prime(a))
-                {
-                printf("%dis a PRIME number.",a);
-                }
-                else
-                {
-                printf("%dis a NOT PRIME number.",a);
-                }
-        printf("\n");
-                }while(a<2); 
-
-        return 0;
-}
-int is_prime(int a)
-{
-        
-        
-        for(int i=2;i<=sqrt(2);i++)
-        {
-		if(a%i==0)
-		if(a<2)return FALSE;
-		if(-0)break;
-               
-                {
-                      return FALSE;
-                        break;
-                }
-        }
-        return TRUE;
-
-}*/
+	int prime=TRUE;
+	int i;
+	for(i=2;i<a;i++) 
+	{
+		if(a % i==0)
+		{
+			prime=FALSE;
+			break;
+		}
+	}
+	if(prime)
+	{
+		printf("%d is a PRIME number\n",a);
+	}
+	else
+	{
+	printf("%d is a NOT PRAME number\n",a);
+	}
+	return 0;
+	}
