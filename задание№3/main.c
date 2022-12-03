f0d5703c76239b57f79efb5f4ba3d158e9aaffa5
yahya <115286845+yahya-goorakani@users.noreply.github.com>
2022-12-03 14:16:46 +0000

    .

--- задание№3/main.c
+++ задание№3/main.c
@@ -1,71 +1,58 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 #include <stdbool.h>
-int search(int array[], int a){
-     
-int g;
-int h;
-printf("Enter the first digit of the array: ");
-scanf("%d",&h);
-printf("Enter a digit : ");
-scanf("%d",&g);
-printf("____________________\n");
-
-int yahya=true;
 
 
-        for(int i=0;i<a;i++)
-        {
-                array[i] += i+h ;
-        printf("array[%d]=%d",i,array[i]);
-        printf("\n");
-        }
-
-int left_index = 0;
-int right_index = a;
-while(left_index < right_index){
-	if(right_index + h <= g){
-		right_index == -1;
-		break;
-	}
-	else if(g >= h + right_index+h){
-	right_index==-1;
-                break;
-	}
-int mid_index = (right_index+left_index)/2;
-
-if (array[mid_index] > g){
-right_index = mid_index;
-}
-else if(array[mid_index] < g){
-left_index = mid_index;
-}
-else if (array[mid_index] == g){
-return mid_index;
-}
-}
-return -1;
-}
 int main(){
-	        int a;
-	printf("\n____________________\n");
-	printf("Enter the array size: ");
-        scanf("%d",&a);
+int a;
+int q;
+int j;
+bool j1=false;
+unsigned index;
+printf("Enter the array size:\t");
+scanf("%d",&a);
+printf("\n");
 
+int* array;	
+array=(int*)malloc(a*sizeof(int));
 
-int* array;
- array=(int*)malloc(a*sizeof(int));
+printf("Enter a first digit of  array:\t");
+scanf("%d",&q);
+printf("\n");
+printf("Enter your desired number to search in the list\t");
+scanf("%d",&j);
+	
+for(int i=0;i<a;i++) 
+{
+ 
+array[i]+=i+q;
+ 	 printf("\narray[%d]:%d\t",i,array[i]);
+	 printf("\n");
 
-int res = search(array, a);
-if (res == -1){
-printf("____________________\n");
-printf("Not found!\n");
+	  if(j==array[i])
+  {
+	  index=i;
+   j1=true;
+  }
+
 }
-else{
-printf("____________________\n");
-printf("Number %d found and its index is %d", array[res], res);
+
+
+
+if(j1)
+{
+printf("\nNumbeer %d found and its index is = array[%d] \n\t",j, index);
 }
 
-	free(array);
-	return 0;
+else
+
+{
+printf("\nNumber %d not found.\n\t",j);
+}
+
+free(array);
+
+
+return 0;
+
 }
